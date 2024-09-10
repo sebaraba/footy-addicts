494da49d8c4c79d346d74552c1254da33376a801
rabas <rabas@haufe.com>
2024-09-11 00:24:45 +0300

    feat(app.backend): update format prettier config

--- apps/app.backend/src/index.ts
+++ apps/app.backend/src/index.ts
@@ -24,9 +24,13 @@ app.use('/bookings', bookingRoutes);
 mongoose
   .connect('mongodb://localhost:27017/football_booking')
   .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+  .catch((err) =>
+    console.error('MongoDB connection error:', err)
+  );
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(
+    `Server is running on http://localhost:${port}`
+  );
 });
