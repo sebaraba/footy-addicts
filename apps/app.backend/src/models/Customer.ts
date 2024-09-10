73bc0789cc063e7fa490f008ed0ecd93c5a216d8
rabas <rabas@haufe.com>
2024-09-10 23:16:04 +0300

    feat(app.backend): add initial data models

---
+++ apps/app.backend/src/models/Customer.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface ICustomer extends Document {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+const CustomerSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true }
+});
+
+export default mongoose.model<ICustomer>('Customer', CustomerSchema);
