97736536d21aa812a581a5a7543ff0968e7fab37
sebaraba <rabasebastians@gmail.com>
2024-09-20 20:03:26 +0300

    feat(Router): add react router for the application

--- apps/app.frontend/src/main.tsx
+++ apps/app.frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App.tsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -7,7 +8,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </StrictMode>,
 );
