ea5e0f55f12928f42c1ae93ff0337348d373823c
sebaraba <rabasebastians@gmail.com>
2024-09-23 00:27:32 +0300

    feat(Login): add suport for Login and new pages

---
+++ apps/app.frontend/src/services/authService.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const API_URL = process.env.VITE_API_AUTH_URL ?? "/https/auth";
+
+export const register = async (userData: any) => {
+  try {
+    const response = await axios.post(`${API_URL}/register`, userData);
+    return response.data;
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw error;
+  }
+};
+
+export const login = async (userData: any) => {
+  try {
+    const response = await axios.post(`${API_URL}/login`, userData);
+    const { token } = response.data;
+    localStorage.setItem("token", token);
+    return response.data;
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    throw error;
+  }
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+};
+
+export const getToken = () => {
+  return localStorage.getItem("token");
+};
+
+export const isAuthenticated = () => {
+  const token = getToken();
+  return !!token;
+};
