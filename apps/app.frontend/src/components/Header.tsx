1a4bb50f7cb70ac9d10aad7e2171cd2db8d98b98
sebaraba <rabasebastians@gmail.com>
2024-09-22 21:56:19 +0300

    chore(Header): refactor header component

--- apps/app.frontend/src/components/Header.tsx
+++ apps/app.frontend/src/components/Header.tsx
@@ -1,8 +1,15 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 
 export const Header = () => {
   return (
-    <Box as="header" w="full" bg="gray.800" color="white" p={4}>
+    <Box
+      as="header"
+      w="full"
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+      width={"100%"}
+      p={4}
+    >
       <h1>Header</h1>
     </Box>
   );
