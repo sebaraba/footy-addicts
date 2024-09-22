64083e8d528a130cf2cba1e848ee15d7a1635697
sebaraba <rabasebastians@gmail.com>
2024-09-22 21:56:54 +0300

    feat(FERequests): add register request for frontend

--- apps/app.frontend/src/App.tsx
+++ apps/app.frontend/src/App.tsx
@@ -1,24 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import { Container, Flex } from "@chakra-ui/react";
-import { LargeWithAppLinksAndSocial } from "./components/Footer.tsx";
+import { Footer } from "./components/Footer.tsx";
+import { Header } from "./components/Header.tsx";
 import HomePage from "./pages/HomePage.tsx";
 import RegistrationForm from "./pages/RegistrationForm.tsx";
 
 export const App = () => (
   <Container maxW="100vw" w="100%" padding={0}>
-    <header style={{ width: "100%", position: "fixed" }}>
-      <h1>Header</h1>
-    </header>
-    <Flex
-      h="100vh"
-      py={[0, 10, 20]}
-      direction={{ base: "column-reverse", md: "row" }}
-    >
+    <Header />
+    <Flex py={[0, 10, 20]} direction={{ base: "column-reverse", md: "row" }}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegistrationForm />} />
       </Routes>
     </Flex>
-    <LargeWithAppLinksAndSocial />
+    <Footer />
   </Container>
 );
--- apps/app.frontend/src/pages/RegistrationForm.tsx
+++ apps/app.frontend/src/pages/RegistrationForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import axios from "axios";
 import {
   VStack,
   GridItem,
@@ -5,7 +7,6 @@ import {
   FormLabel,
   Input,
   Select,
-  Checkbox,
   Button,
   SimpleGrid,
   useBreakpointValue,
@@ -17,6 +18,31 @@ import {
 const RegistrationForm = () => {
   const colSpan = useBreakpointValue({ base: 2, md: 1 });
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("usa");
+
+  const handleSubmit = async () => {
+    const customerData = {
+      firstName,
+      lastName,
+      address,
+      city,
+      country,
+    };
+
+    console.log("Customer data:", customerData);
+
+    try {
+      const response = await axios.post("/https/customers", customerData);
+      console.log("Customer created:", response.data);
+    } catch (error) {
+      console.error("Error creating customer:", error);
+    }
+  };
+
   return (
     <Container centerContent={true}>
       <Heading size="2xl">Your details</Heading>
@@ -26,35 +52,54 @@ const RegistrationForm = () => {
           <GridItem colSpan={colSpan}>
             <FormControl>
               <FormLabel>First Name</FormLabel>
-              <Input placeholder="John" />
+              <Input
+                placeholder="John"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
             </FormControl>
           </GridItem>
 
           <GridItem colSpan={colSpan}>
             <FormControl>
               <FormLabel>Last Name</FormLabel>
-              <Input placeholder="Doe" />
+              <Input
+                placeholder="Doe"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
             </FormControl>
           </GridItem>
 
           <GridItem colSpan={2}>
             <FormControl>
               <FormLabel>Address</FormLabel>
-              <Input placeholder="Blvd. Broken Dreams 21" />
+              <Input
+                placeholder="Blvd. Broken Dreams 21"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
             </FormControl>
           </GridItem>
 
           <GridItem colSpan={colSpan}>
             <FormControl>
               <FormLabel>City</FormLabel>
-              <Input placeholder="San Francisco" />
+              <Input
+                placeholder="San Francisco"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
             </FormControl>
           </GridItem>
 
           <GridItem colSpan={colSpan}>
             <FormControl>
-              <FormLabel>City</FormLabel>
-              <Select>
+              <FormLabel>Country</FormLabel>
+              <Select
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+              >
                 <option value="usa">United States of America</option>
                 <option value="uae">United Arab Emirates</option>
                 <option value="nmk">North Macedonia</option>
@@ -63,13 +108,23 @@ const RegistrationForm = () => {
             </FormControl>
           </GridItem>
 
-          <GridItem colSpan={2}>
-            <Checkbox defaultChecked>Ship to billing address.</Checkbox>
-          </GridItem>
+          {/*<GridItem colSpan={2}>*/}
+          {/*  <Checkbox*/}
+          {/*    isChecked={shipToBilling}*/}
+          {/*    onChange={(e) => setShipToBilling(e.target.checked)}*/}
+          {/*  >*/}
+          {/*    Ship to billing address.*/}
+          {/*  </Checkbox>*/}
+          {/*</GridItem>*/}
 
           <GridItem colSpan={2}>
-            <Button size="lb" w="full">
-              Place Order
+            <Button
+              size="lg"
+              w="full"
+              colorScheme="blue"
+              onClick={handleSubmit}
+            >
+              Register
             </Button>
           </GridItem>
         </SimpleGrid>
