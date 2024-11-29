977b0c77955034b685bbd49edc42d6e3f7d7046d
sebaraba <rabasebastians@gmail.com>
2024-11-29 21:39:59 +0200

    fix: lint

--- apps/app.frontend/src/pages/PlayerProfilePage.tsx
+++ apps/app.frontend/src/pages/PlayerProfilePage.tsx
@@ -1,18 +1,14 @@
 import { Box, Container } from "@chakra-ui/react";
-import PlayerProfile from "/Users/macbook/Desktop/footy-addicts/apps/app.frontend/src/components/Profile/PlayerProfile.tsx";
+import PlayerProfile from "../components/Profile/PlayerProfile.tsx";
 
 export const PlayerProfilePage = () => {
-
-    return(
-
-		<Container centerContent={true}>
-			<Box bg="#F6F6F6" w="1000px" p="8" borderRadius="md" boxShadow="md">
-                <PlayerProfile/>
-            </Box>
-        </Container>
-        
-    
-      );
-}
+  return (
+    <Container centerContent={true}>
+      <Box bg="#F6F6F6" w="1000px" p="8" borderRadius="md" boxShadow="md">
+        <PlayerProfile />
+      </Box>
+    </Container>
+  );
+};
 
 export default PlayerProfilePage;
